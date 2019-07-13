2f7d1d12c5be24335205c0b79e98076b6d3a78c5
Justin Perrie <j_perrie174@hotmail.com>
2019-07-14 01:03:46 +1000

    Added Buffer Layout

--- Centurion/src/Centurion/Application.cpp
+++ Centurion/src/Centurion/Application.cpp
@@ -12,6 +12,27 @@ namespace Centurion {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Centurion::ShaderDataType::Float:		return GL_FLOAT;
+			case Centurion::ShaderDataType::Float2:		return GL_FLOAT;
+			case Centurion::ShaderDataType::Float3:		return GL_FLOAT;
+			case Centurion::ShaderDataType::Float4:		return GL_FLOAT;
+			case Centurion::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Centurion::ShaderDataType::Mat4:		return GL_FLOAT;
+			case Centurion::ShaderDataType::Int:		return GL_INT;
+			case Centurion::ShaderDataType::Int2:		return GL_INT;
+			case Centurion::ShaderDataType::Int3:		return GL_INT;
+			case Centurion::ShaderDataType::Int4:		return GL_INT;
+			case Centurion::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		CTN_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		s_Instance = this;
@@ -24,45 +45,56 @@ namespace Centurion {
 
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
-
-		//glGenBuffers(1, &m_VertexBuffer);
-		//glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
-			
-		float vertices[3 * 3] = { // Data is in the CPU
-			-0.5, -0.5f, 0.0f,
-			0.5, -0.5f, 0.0f,
-			0.0, 0.5f, 0.0f,
+					
+		float vertices[3 * 7] = { // Data is in the CPU
+			-0.5, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+			0.5, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+			0.0, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
 		};
 
 		// Now, lets move the data to the GPU
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-
-		//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // GL_STATIC_DRAW (Draw once)
-
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr); // Structure the data
 		
-		//glGenBuffers(1, &m_IndexBuffer);
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer); // Setup the Order of drawing
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position"},
+				{ShaderDataType::Float4, "a_Color"}
+			};
+			
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
 
 		
+		for (const auto& element : m_VertexBuffer->GetLayout())
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.Type), element.Normalized ? GL_TRUE : GL_FALSE, m_VertexBuffer->GetLayout().GetStride(), (const void*)element.Offset); // Structure the data
+			index++;
+		}
+
+		//glEnableVertexAttribArray(0);
+		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr); // Structure the data
+				
 		uint32_t indices[3] = { 0, 1, 2 }; // Order of drawing
 
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) /  sizeof(uint32_t)));
-
-		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 		std::string vertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position + 0.5;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position,1);
 			}
 		)";
@@ -73,10 +105,12 @@ namespace Centurion {
 			layout(location = 0) out vec4 colour;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{				
 				colour = vec4(v_Position, 1.0);
+				colour = v_Color;
 			}
 		)";
 
--- Centurion/src/Platform/OpenGL/OpenGLBuffer.h
+++ Centurion/src/Platform/OpenGL/OpenGLBuffer.h
@@ -8,12 +8,16 @@ namespace Centurion
 	{
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	public:
-		OpenGLVertexBuffer(float* vertices, uint32_t size);
+		OpenGLVertexBuffer(float* vertics, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) { m_Layout = layout; }
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -25,8 +29,8 @@ namespace Centurion
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t size);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 	
 		virtual uint32_t GetCount() const { return m_Count; }
 	};
